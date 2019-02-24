c98425fb52b2636e92a2b212ae3b19fe2e3e387d
agh372 <arjunhegde372@gmail.com>
2019-02-23 23:49:11 -0600

    added fragments

---
+++ app/src/main/java/com/teamfitness/fitapp/model/Coupon.java
@@ -0,0 +1,22 @@
+package com.teamfitness.fitapp.model;
+
+public class Coupon {
+    private String discount;
+    private String name;
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
